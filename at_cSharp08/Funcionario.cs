d0b4915e4786f2afbd427645da46598c303f0592
Isabella Mayer <bella@MacBook-Pro-de-Isabella.local>
2025-04-13 15:55:25 -0300

    salario

---
+++ at_cSharp08/Funcionario.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Funcionario
+{
+    public string Nome { get; set; }
+    public string Cargo { get; set; }
+    public decimal SalarioAntes { get; set; }
+
+    public Funcionario(string nome, string cargo, decimal salarioantes)
+    {
+        Nome = nome;
+        Cargo = cargo;
+        SalarioAntes = salarioantes;
+    }
+
+    public virtual decimal CalcularSalario()
+    {
+        return SalarioAntes;
+    }
+
+    public virtual void MostrarInfo()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Cargo: {Cargo}");
+        Console.WriteLine("Salário: R$ " + CalcularSalario().ToString("F2")
+);
+    }
+}
+
+
+class Gerente : Funcionario
+{
+    public Gerente(string nome, decimal salarioantes)
+        : base(nome, "Gerente", salarioantes) { }
+
+    public override decimal CalcularSalario()
+    {
+        return SalarioAntes * 1.2m; 
+    }
+}
---
+++ at_cSharp08/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Funcionario funcionario = new Funcionario("isabella", "dev", 25000);
+        funcionario.MostrarInfo();
+
+
+        Gerente gerente = new Gerente("cintia", 42000);
+        gerente.MostrarInfo();
+    }
+}
